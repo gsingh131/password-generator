7fef3047823eab5638ba95fa743ae1c6f4d4dac9
gsingh131 <gaganajitsingh131@gmail.com>
2024-02-23 15:20:27 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passwordOne = document.getElementById("password-el-one")
+let passwordTwo = document.getElementById("password-el-two")
+
+function generatePasswords(){
+    passwordOne.textContent = ""
+    for(let i = 0; i<=14; i++){
+        let randomPassword = characters[Math.floor(Math.random()* characters.length)]
+        passwordOne.textContent += randomPassword
+    }
+    
+    passwordTwo.textContent = ""
+    for(let i = 0; i<=14; i++){
+        let randomPassword = characters[Math.floor(Math.random()* characters.length)]
+        passwordTwo.textContent += randomPassword
+    }
+}   
+
+
+
+
+
+
+
