289e24f5e8a07a621ec368a00ce8ceb6f0e0267d
gsingh131 <gaganajitsingh131@gmail.com>
2024-03-17 16:36:47 -0400

    fixed issues for vite
    replaced onclick with addeventlistener

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let passwordOne = document.getElementById("password-el-one")
-let passwordTwo = document.getElementById("password-el-two")
+const passwordOne = document.getElementById("password-el-one")
+const passwordTwo = document.getElementById("password-el-two")
+const passwordBtn = document.getElementById("password-btn")
 
+passwordBtn.addEventListener("click" , function(){
+    generatePasswords()
+})
 
 function generatePasswords(){
     passwordOne.textContent = ""
